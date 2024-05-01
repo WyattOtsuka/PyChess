02de502348faa25ff4b5308ec51855be426c0629
Otsuka <wotsuka@amazon.com>
2024-05-01 15:34:34 -0700

    adding fen converter and tests

---
+++ test_fen_converter.py
@@ -0,0 +1,20 @@
+import unittest 
+from fen_converter import board_to_fen
+
+class TestKnightMoves(unittest.TestCase):
+    def test_starting_position(self):
+        board = [
+            ['b_r', 'b_n', 'b_b', 'b_q', 'b_k', 'b_b', 'b_n', 'b_r'],
+            ['b_p', 'b_p', 'b_p', 'b_p', 'b_p', 'b_p', 'b_p', 'b_p'],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['', '', '', '', '', '', '', ''],
+            ['w_p', 'w_p', 'w_p', 'w_p', 'w_p', 'w_p', 'w_p', 'w_p'],
+            ['w_r', 'w_n', 'w_b', 'w_q', 'w_k', 'w_b', 'w_n', 'w_r']
+        ]
+        self.assertTrue(board_to_fen(board) == "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
+
+
+if __name__ == '__main__':
+    unittest.main()
