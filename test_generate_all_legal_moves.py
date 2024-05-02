8acfa1c53fadea8ae5b831712e4306c8bafd5bd9
Otsuka <wotsuka@amazon.com>
2024-05-01 20:05:21 -0700

    Refactoring base case, adding fen output and nodes searched at different depths

--- test_generate_all_legal_moves.py
+++ test_generate_all_legal_moves.py
@@ -1,4 +1,5 @@
 import move_generator
+import fen_converter
 
 board_1 = [
     ['b_r', 'b_n', 'b_b', 'b_q', '', 'b_k', '', 'b_r'],
@@ -25,13 +26,17 @@ board = [
 squares_to_moves = move_generator.generate_all_legal_moves_for_color(board, 'w')
 
 num_moves = 0
-for square in squares_to_moves:
-    num_moves += len(squares_to_moves[square])
 
-print(num_moves)
 
-def calculate_moves(squares_to_moves, depth_to_go, color='b'):
+def calculate_moves(squares_to_moves, depth_to_go, color='b', print_nodes = False):
     num_moves = 0
+    if depth_to_go == 0:
+        for square in squares_to_moves:
+            num_moves += len(squares_to_moves[square])
+
+        return num_moves
+
+
     for square in squares_to_moves:
         # Make the move
         for move in squares_to_moves[square]:
@@ -41,18 +46,23 @@ def calculate_moves(squares_to_moves, depth_to_go, color='b'):
 
             board[move[0]][move[1]] = board[int(square[0])][int(square[2])]
             board[int(square[0])][int(square[2])] = ''
-            squares_to_moves_2 = move_generator.generate_all_legal_moves_for_color(board, color, en_passant_square=en_passant_square)
 
-            if depth_to_go == 0:
-                for square_2 in squares_to_moves_2:
-                    num_moves += len(squares_to_moves_2[square_2])
-            else:
-                num_moves += calculate_moves(squares_to_moves_2, depth_to_go - 1, 'w' if color == 'b' else 'b')
-            
+
+            squares_to_moves_2 = move_generator.generate_all_legal_moves_for_color(board, 'w' if color == 'b' else 'b', en_passant_square=en_passant_square)
+            new_move_count = calculate_moves(squares_to_moves_2, depth_to_go - 1, 'w' if color == 'b' else 'b')
+            if print_nodes:
+               print(f"{chr(97+int(square[2]))}{8-int(square[0])}{chr(97+int(move[1]))}{8-move[0]}: {new_move_count}  --- {fen_converter.board_to_fen(board)} {'w' if color == 'b' else 'b'}")
+
+            num_moves += new_move_count
+
             board[int(square[0])][int(square[2])] = board[move[0]][move[1]]
             board[move[0]][move[1]] = ''
 
     return num_moves
 
-# print(calculate_moves(squares_to_moves, 0))
-print(calculate_moves(squares_to_moves, 1)) 
+print(fen_converter.board_to_fen(board))
+# print(calculate_moves(squares_to_moves, 0, color='w'))
+
+# print(calculate_moves(squares_to_moves, 1, color='w'))
+
+print(calculate_moves(squares_to_moves, 2, color='w', print_nodes=True))
