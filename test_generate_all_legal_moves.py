e9df803d0fc07e65b0cc60b3c29e8754902d44a4
Wyatt Otsuka <wotsuka@amazon.com>
2024-05-10 13:28:58 -0700

    Adding unmake_move and more castling logic

--- test_generate_all_legal_moves.py
+++ test_generate_all_legal_moves.py
@@ -57,7 +57,7 @@ def calculate_moves(board, squares_to_moves, depth_to_go, color='b', print_nodes
         # Make the move
         for move in squares_to_moves[square]:
             rook_moved_status = {'w_rook_a_moved': w_rook_a_moved, 'w_rook_h_moved': w_rook_h_moved, 'b_rook_a_moved': b_rook_a_moved, 'b_rook_h_moved': b_rook_h_moved}
-            en_passant_square, was_en_passant, prev_rook_a_status, prev_rook_h_status, next_turn_castling, destination_piece= make_move(board, square, move, color, rook_moved_status)
+            en_passant_square, was_en_passant, was_castle, prev_rook_a_status, prev_rook_h_status, next_turn_castling, destination_piece = make_move(board, square, move, color, rook_moved_status)
 
             squares_to_moves_2 = move_generator.generate_all_legal_moves_for_color(board, opposite_color(color), en_passant_square=en_passant_square, castling=next_turn_castling, exit_early=True)
             new_move_count = calculate_moves(board, squares_to_moves_2, depth_to_go - 1, opposite_color(color))
@@ -73,27 +73,42 @@ def calculate_moves(board, squares_to_moves, depth_to_go, color='b', print_nodes
                 b_rook_a_moved = prev_rook_a_status
                 b_rook_h_moved = prev_rook_h_status
 
-            if was_en_passant:
-                board[int(square[0])][move[1]] = destination_piece
-                board[int(square[0])][int(square[2])] = board[move[0]][move[1]]
-                board[move[0]][move[1]] = ''
-            else:
-                if len(move) == 3:
-                    board[int(square[0])][int(square[2])] = board[move[0]][move[1]][:2] + "p"
-                else:
-                    board[int(square[0])][int(square[2])] = board[move[0]][move[1]]
-                board[move[0]][move[1]] = destination_piece
+            unmake_move(board, square, move, was_castle, was_en_passant, destination_piece)
 
     return num_moves
 
-def make_move(board, square, move, color, rook_moved_status):
-    en_passant_square = get_en_passant_square(board, square, move)
-    was_en_passant, destination_piece = move_piece(board, square, move)
+def unmake_move(board, start_square, move, was_castle, was_en_passant, destination_piece):
+    if was_castle:
+        board[move[0]][KING_FILE] = board[move[0]][move[1]]
+        if move[1] == 6:
+            board[move[0]][H_FILE] = board[move[0]][5]
+            board[move[0]][5] = ''
+        else:
+            board[move[0]][A_FILE] = board[move[0]][3]
+            board[move[0]][3] = ''
+
+        board[move[0]][move[1]] = ''
+
+    elif was_en_passant:
+        board[int(start_square[0])][move[1]] = destination_piece
+        board[int(start_square[0])][int(start_square[2])] = board[move[0]][move[1]]
+        board[move[0]][move[1]] = ''
+
+    else:
+        if len(move) == 3:
+            board[int(start_square[0])][int(start_square[2])] = board[move[0]][move[1]][:2] + "p"
+        else:
+            board[int(start_square[0])][int(start_square[2])] = board[move[0]][move[1]]
+        board[move[0]][move[1]] = destination_piece
+
+def make_move(board, start_square, move, color, rook_moved_status):
+    en_passant_square = get_en_passant_square(board, start_square, move)
+    was_en_passant, was_castle, destination_piece = move_piece(board, start_square, move)
 
     prev_rook_a_status, prev_rook_h_status = update_rook_status(rook_moved_status, color, move)
     next_turn_castling = get_next_turn_castling(board, opposite_color(color), rook_moved_status)
 
-    return en_passant_square, was_en_passant, prev_rook_a_status, prev_rook_h_status, next_turn_castling, destination_piece
+    return en_passant_square, was_en_passant, was_castle, prev_rook_a_status, prev_rook_h_status, next_turn_castling, destination_piece
 
 def get_en_passant_square(board, start_square, end_square):
     if board[int(start_square[0])][int(start_square[2])] != '' and board[int(start_square[0])][int(start_square[2])][2] == 'p' and abs(int(start_square[0]) - end_square[0]) == 2:
@@ -103,13 +118,28 @@ def get_en_passant_square(board, start_square, end_square):
 
 def move_piece(board, start_square, end_square):
     was_en_passant = False
+    was_castle = False
     destination_piece = board[end_square[0]][end_square[1]]
 
-    if board[int(start_square[0])][int(start_square[2])][2] == 'p' and abs(int(start_square[2]) - end_square[1]) == 1 and board[end_square[0]][end_square[1]] == '':
+    if board[int(start_square[0])][int(start_square[2])][2] == 'k' and abs(int(start_square[2]) - end_square[1]) == 2:
+        was_castle = True
+        if end_square[1] == 6:
+            board[end_square[0]][(int(start_square[2]) + end_square[1]) // 2] = board[end_square[0]][H_FILE]
+            board[end_square[0]][H_FILE] = ''
+
+        if end_square[1] == 2:
+            board[end_square[0]][(int(start_square[2]) + end_square[1]) // 2] = board[end_square[0]][A_FILE]
+            board[end_square[0]][A_FILE] = ''
+
+        board[end_square[0]][end_square[1]] = board[int(start_square[0])][int(start_square[2])]
+        board[int(start_square[0])][int(start_square[2])] = ''
+
+    elif board[int(start_square[0])][int(start_square[2])][2] == 'p' and abs(int(start_square[2]) - end_square[1]) == 1 and board[end_square[0]][end_square[1]] == '':
         was_en_passant = True
         board[end_square[0]][end_square[1]] = board[int(start_square[0])][int(start_square[2])]
         board[int(start_square[0])][end_square[1]] = ''
         board[int(start_square[0])][int(start_square[2])] = ''
+        
     else:
         if len(end_square) == 3:
             board[end_square[0]][end_square[1]] = board[int(start_square[0])][int(start_square[2])][:2] + end_square[2]
@@ -117,7 +147,7 @@ def move_piece(board, start_square, end_square):
             board[end_square[0]][end_square[1]] = board[int(start_square[0])][int(start_square[2])]
         board[int(start_square[0])][int(start_square[2])] = ''
 
-    return was_en_passant, destination_piece
+    return was_en_passant, was_castle, destination_piece
 
 def update_rook_status(rook_moved_status, color, end_square):
     prev_rook_a_status = None
